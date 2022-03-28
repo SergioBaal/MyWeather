35d283e861b9f2edcb6d33c7b62b343afc69bee7
SergioBaal <98283020+SergioBaal@users.noreply.github.com>
2022-03-28 15:32:19 +0300

    Стартую приложение

---
+++ app/src/main/java/ru/geekbrains/myweather/view/MainFragment.kt
@@ -0,0 +1,24 @@
+package ru.geekbrains.myweather.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.geekbrains.myweather.R
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
