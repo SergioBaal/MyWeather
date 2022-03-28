d20969e706258b8037b9542ffb0bc80305c7bf38
SergioBaal <98283020+SergioBaal@users.noreply.github.com>
2022-03-28 16:22:02 +0300

    Созданы view и viewModel. viewModel проверяет жизненный цикл активити.  У viewModel вызывается getWeather. Стартует поток-заглушка. В LiveData записывается значение, она обновляется, MainFragment подписан на ее обновления, вызывается метод запускающий Toast

--- app/src/main/java/ru/geekbrains/myweather/view/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/myweather/view/MainFragment.kt
@@ -5,16 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import ru.geekbrains.myweather.R
+import ru.geekbrains.myweather.viewmodel.MainViewModel
 
 
 class MainFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val observer = object : Observer<Any> {
+            override fun onChanged(data: Any) {
+                renderData(data)
+            }
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(requireContext(), "Работает", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
--- app/src/main/java/ru/geekbrains/myweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/myweather/viewmodel/MainViewModel.kt
@@ -1,4 +1,21 @@
 package ru.geekbrains.myweather.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
+
+class MainViewModel(private val liveData:MutableLiveData<Any> = MutableLiveData()):ViewModel() {
+
+    fun getData (): LiveData <Any>{
+        return liveData
+    }
+
+    fun getWeather() {
+        Thread{
+            sleep(2000L)
+            liveData.postValue(Any())
+        }.start()
+    }
+
 }
